71b1289cf8edaddb7b04e13ba8aaf65a05c5143f
mc_fdc <Masato.04.11.2007@gmail.com>
2022-11-30 21:12:10 +0900

    add: hav

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import Link from 'next/link'
 import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css'
+import { GoMarkGithub } from "react-icons/go"
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
@@ -10,8 +12,13 @@ function MyApp({ Component, pageProps }: AppProps) {
             <Head>
                 <title>Discord webhook</title>
             </Head>
-            <header className="p-2 bg-[#5865F2]">
+            <header className="p-2 bg-[#5865F2] flex justify-between">
                 <h1 className="text-3xl text-white">Webhooked</h1>
+                <nav className="flex">
+                    <Link href="https://github.com/tuna2134/webhook-and-embed">
+                        <a><GoMarkGithub /></a>
+                    </Link>
+                </nav>
             </header>
             <main className="px-5 py-4">
                 <Component {...pageProps} />
